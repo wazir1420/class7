8633d06f6c1f76b3fa9e1628ef54382a9b5e88d5
HuzaifaRizwan15 <huzaifahunter04@gmail.com>
2024-09-22 14:06:12 +0500

    done

--- classes.dart
+++ classes.dart
@@ -14,6 +14,9 @@ void main() {
   Teacher teacher = Teacher();
   teacher.name = 'John Doe';
   print(teacher.name);
+  teacher.subject.addAll(['flutter', 'Mern', 'stact']);
+  print(teacher.canTeach()); // true
+  print(teacher.subject);
 
   Teacher teacher1 = Teacher();
   teacher1.name = 'Jane Doe';
